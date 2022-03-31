049fa83014e7408339417c88a63af27cfd73ad0f
kujo jotaro <jotaro@jojo.exchange>
2022-03-31 12:02:16 +0800

    add test mark price source event

--- contracts/testSupport/TestMarkPriceSource.sol
+++ contracts/testSupport/TestMarkPriceSource.sol
@@ -11,6 +11,10 @@ pragma experimental ABIEncoderV2;
 
 contract TestMarkPriceSource is Ownable {
     uint256 public price;
+    uint256 public updatedAt;
+    uint256 public roundId;
+
+    event AnswerUpdated(int256 indexed current, uint256 indexed roundId, uint256 updatedAt);
 
     function getMarkPrice() external view returns (uint256) {
         return price;
@@ -18,5 +22,8 @@ contract TestMarkPriceSource is Ownable {
 
     function setMarkPrice(uint256 newPrice) external onlyOwner {
         price = newPrice;
+        roundId ++;
+        updatedAt = block.timestamp;
+        emit AnswerUpdated(int256(price), roundId, updatedAt);
     }
 }

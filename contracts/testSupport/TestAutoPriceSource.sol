c4f0b4b5061daa2c7611337e124770549ab38575
kujo jotaro <jotaro@jojo.exchange>
2022-08-10 21:19:00 +0700

    add liqusdc test market

---
+++ contracts/testSupport/TestAutoPriceSource.sol
@@ -0,0 +1,41 @@
+/*
+    Copyright 2022 JOJO Exchange
+    SPDX-License-Identifier: Apache-2.0
+    ONLY FOR TEST
+    DO NOT DEPLOY IN PRODUCTION ENV
+*/
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+pragma solidity 0.8.9;
+pragma experimental ABIEncoderV2;
+
+contract TestAutoPriceSource is Ownable {
+    uint256 public price;
+    uint256 public updatedAt;
+    uint256 public roundId;
+
+    event AnswerUpdated(
+        int256 indexed current,
+        uint256 indexed roundId,
+        uint256 updatedAt
+    );
+
+    function getMarkPrice() external view returns (uint256) {
+        // Change 1% every minute
+        // -50% -> 50% -> -50%
+        // 0 -> 100min -> 200min
+        uint256 timeOffset = (block.timestamp / 60) % 200; // min
+        if (timeOffset <= 100) {
+            return price / 2 + (timeOffset * price) / 100;
+        } else {
+            return price + price / 2 - ((timeOffset - 100) * price) / 100;
+        }
+    }
+
+    function setMarkPrice(uint256 newPrice) external onlyOwner {
+        price = newPrice;
+        roundId++;
+        updatedAt = block.timestamp;
+        emit AnswerUpdated(int256(price), roundId, updatedAt);
+    }
+}

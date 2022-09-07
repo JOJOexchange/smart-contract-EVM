5fde6e6eaf88ef752273dd6d4f5ad9c353e06996
kujo jotaro <jotaro@jojo.exchange>
2022-09-07 16:03:59 +0700

    add const oracle

---
+++ contracts/adaptor/constOracle.sol
@@ -0,0 +1,21 @@
+/*
+    Copyright 2022 JOJO Exchange
+    SPDX-License-Identifier: Apache-2.0
+*/
+
+pragma solidity 0.8.9;
+pragma experimental ABIEncoderV2;
+
+contract ConstOracle {
+    uint256 public immutable price;
+
+    constructor(
+        uint256 _price
+    ) {
+        price = _price;
+    }
+
+    function getMarkPrice() external view returns (uint256) {
+        return price;
+    }
+}

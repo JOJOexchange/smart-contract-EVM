90b7ab0cfa14a0442876a3ce92f26fc909f05572
kujo jotaro <jotaro@jojo.exchange>
2022-10-10 22:48:59 +0700

    add funding rate update limiter

--- test/fundingRate-test.ts
+++ test/fundingRate-test.ts
@@ -1,9 +1,11 @@
 import "./utils/hooks";
-import { Wallet, utils } from "ethers";
+import { ethers } from "hardhat";
+import { Contract, Wallet, utils } from "ethers";
 import { expect } from "chai";
 import { basicContext, Context } from "../scripts/context";
 import { getDefaultOrderEnv, openPosition, OrderEnv } from "../scripts/order";
 import { checkBalance } from "./utils/checkers";
+import { timeJump } from "./utils/timemachine";
 
 /*
   Test cases list
@@ -170,5 +172,48 @@ describe("Funding rate", () => {
         [utils.parseEther("0.5"), utils.parseEther("1")]
       )
     ).to.be.revertedWith("JOJO_ARRAY_LENGTH_NOT_SAME");
+  });
+
+  it.only("limiter", async () => {
+    let limiter: Contract = await (
+      await ethers.getContractFactory("FundingRateUpdateLimiter")
+    ).deploy(context.dealer.address, 3);
+
+    let perps = [
+      context.perpList[0].address,
+      context.perpList[1].address,
+      context.perpList[2].address,
+    ];
+    await context.dealer.setFundingRateKeeper(limiter.address);
+    await limiter.updateFundingRate(perps, [
+      utils.parseEther("1"),
+      utils.parseEther("2"),
+      utils.parseEther("3"),
+    ]);
+    expect(await context.perpList[0].getFundingRate()).to.be.equal(utils.parseEther("1"))
+    expect(await context.perpList[1].getFundingRate()).to.be.equal(utils.parseEther("2"))
+    expect(await context.perpList[2].getFundingRate()).to.be.equal(utils.parseEther("3"))
+
+    await timeJump(86400);
+    expect(await limiter.getMaxChange(context.perpList[0].address)).to.be.equal(utils.parseEther("2700"))
+    expect(await limiter.getMaxChange(context.perpList[1].address)).to.be.equal(utils.parseEther("300"))
+    expect(await limiter.getMaxChange(context.perpList[2].address)).to.be.equal(utils.parseEther("3"))
+
+    await expect(limiter.updateFundingRate([context.perpList[0].address], [utils.parseEther("2702")])).to.be.revertedWith("FUNDING_RATE_CHANGE_TOO_MUCH")
+    await expect(limiter.updateFundingRate([context.perpList[1].address], [utils.parseEther("303")])).to.be.revertedWith("FUNDING_RATE_CHANGE_TOO_MUCH")
+    await expect(limiter.updateFundingRate([context.perpList[2].address], [utils.parseEther("7")])).to.be.revertedWith("FUNDING_RATE_CHANGE_TOO_MUCH")
+
+    await expect(limiter.updateFundingRate([context.perpList[0].address], [utils.parseEther("-2700")])).to.be.revertedWith("FUNDING_RATE_CHANGE_TOO_MUCH")
+    await expect(limiter.updateFundingRate([context.perpList[1].address], [utils.parseEther("-299")])).to.be.revertedWith("FUNDING_RATE_CHANGE_TOO_MUCH")
+    await expect(limiter.updateFundingRate([context.perpList[2].address], [utils.parseEther("-1")])).to.be.revertedWith("FUNDING_RATE_CHANGE_TOO_MUCH")
+
+    await limiter.updateFundingRate(perps, [
+      utils.parseEther("2701"),
+      utils.parseEther("302"),
+      utils.parseEther("6"),
+    ]);
+    // console.log(
+    //   await limiter.fundingRateUpdateTimestamp(context.perpList[0].address)
+    // );
   });
 });

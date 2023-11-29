037f6c0a673f0f6d52e774a17c07910df3fe6e13
JoscelynFarr <fushikun307@outlook.com>
2023-11-29 11:10:01 +0800

    update fundingRate

--- contracts/fundingRateKeeper/FundingRateUpdateLimiter.sol
+++ contracts/fundingRateKeeper/FundingRateUpdateLimiter.sol
@@ -20,13 +20,18 @@ contract FundingRateUpdateLimiter is Ownable {
 
     // dealer
     address immutable dealer;
-
+    // max speed multiplier, should be 1/2/3/4/5..., no decimal
+    // funding rate max daily change will be limited to 
+    // speedMultiplier*liquidationThreshold
+    // e.d 3 * 3% = 9%
+    uint8 immutable speedMultiplier;
     // The timestamp of the last funding rate update
     // used to limit the change rate of fundingRate
     mapping(address => uint256) public fundingRateUpdateTimestamp;
 
-    constructor(address _dealer) {
+    constructor(address _dealer, uint8 _speedMultiplier) {
         dealer = _dealer;
+        speedMultiplier = _speedMultiplier;
     }
 
     function updateFundingRate(
@@ -35,8 +40,10 @@ contract FundingRateUpdateLimiter is Ownable {
     ) external onlyOwner {
         for (uint256 i = 0; i < perpList.length;) {
             address perp = perpList[i];
+            int256 oldRate = IPerpetual(perp).getFundingRate();
+            uint256 maxChange = getMaxChange(perp);
             require(
-                (rateList[i]).abs() <= 1e16,
+                (rateList[i] - oldRate).abs() <= maxChange,
                 "FUNDING_RATE_CHANGE_TOO_MUCH"
             );
             fundingRateUpdateTimestamp[perp] = block.timestamp;
@@ -46,5 +53,20 @@ contract FundingRateUpdateLimiter is Ownable {
         }
 
         IDealer(dealer).updateFundingRate(perpList, rateList);
+    }
+
+    // limit funding rate change speed
+    // can not exceed speedMultiplier*liquidationThreshold
+    function getMaxChange(address perp) public view returns (uint256) {
+        Types.RiskParams memory params = IDealer(dealer).getRiskParams(perp);
+        uint256 markPrice = IMarkPriceSource(params.markPriceSource)
+            .getMarkPrice();
+        uint256 timeInterval = block.timestamp -
+            fundingRateUpdateTimestamp[perp];
+        uint256 maxChangeRate = (speedMultiplier *
+            timeInterval *
+            params.liquidationThreshold) / (1 days);
+        uint256 maxChange = (maxChangeRate * markPrice) / Types.ONE;
+        return maxChange;
     }
 }

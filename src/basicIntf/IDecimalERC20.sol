fc8c774fbea7ced6071d95695251c2f1dfc8ceae
JoscelynFarr <fushikun307@outlook.com>
2023-12-13 12:17:12 +0900

    re-construct

---
+++ src/basicIntf/IDecimalERC20.sol
@@ -0,0 +1,10 @@
+/*
+    Copyright 2022 JOJO Exchange
+    SPDX-License-Identifier: BUSL-1.1
+*/
+
+pragma solidity 0.8.9;
+
+interface IDecimalERC20 {
+    function decimals() external returns (uint8);
+}

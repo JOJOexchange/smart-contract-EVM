27f6ac48fd3a7d4a62381cc4766cfa7a873bb334
JoscelynFarr <joscelyn@jojo.exchange>
2024-01-08 11:59:57 +0800

    rename

---
+++ test/mocks/MockERC1271Failed.sol
@@ -0,0 +1,18 @@
+/*
+    Copyright 2022 JOJO Exchange
+    SPDX-License-Identifier: BUSL-1.1
+*/
+
+pragma solidity ^0.8.9;
+
+contract MockERC1271Failed {
+    // add this to be excluded from coverage report
+    function test() public {}
+
+    function isValidSignature(
+        bytes32,
+        bytes memory
+    ) external pure returns (bytes4) {
+        return 0x1626ba72;
+    }
+}

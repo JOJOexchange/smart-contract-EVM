b22555c7d09b0089baf1af96a60a510fdfe907de
JoscelynFarr <fushikun307@outlook.com>
2024-03-28 15:28:07 +0800

    update degen dealer

--- src/DegenDealer.sol
+++ src/DegenDealer.sol
@@ -5,6 +5,7 @@
 
 pragma solidity ^0.8.19;
 
+import "./interfaces/IDealer.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
@@ -19,17 +20,20 @@ contract DegenDealer is Ownable {
     }
 
     address public primaryAsset;
+    address public jojoDealer;
 
     using SafeERC20 for IERC20;
 
     event DegenDeposit(address account, uint256 amount);
-    event DegenWithdraw(address account, uint256 amount);
+    event DegenTransferOut(address account, uint256 amount);
+    event DegenWithdraw(address account, uint256 amount, uint256 id);
     event PositionFinalizeLog(
         address indexed trader, int256 paperAmount, int256 creditAmount, int256 fee, int256 pnl, string perp
     );
 
-    constructor(address _primaryAsset) {
+    constructor(address _primaryAsset, address _jojoDealer) {
         primaryAsset = _primaryAsset;
+        jojoDealer = _jojoDealer;
     }
 
     function deposit(address account, uint256 amount) external {
@@ -37,9 +41,15 @@ contract DegenDealer is Ownable {
         emit DegenDeposit(account, amount);
     }
 
-    function withdraw(address account, uint256 amount) external onlyOwner {
-        IERC20(primaryAsset).safeTransfer(account, amount);
-        emit DegenWithdraw(account, amount);
+    function withdraw(address to, uint256 amount, uint256 id, bool isInternal) external onlyOwner {
+        if (isInternal) {
+            IERC20(primaryAsset).approve(jojoDealer, amount);
+            IDealer(jojoDealer).deposit(amount, 0, to);
+            emit DegenTransferOut(to, amount);
+        } else {
+            IERC20(primaryAsset).safeTransfer(to, amount);
+            emit DegenWithdraw(to, amount, id);
+        }
     }
 
     function batchUpdatePosition(PositionInfo[] memory positionDatas) public onlyOwner {
---
+++ src/DegenDepositHelper.sol
@@ -0,0 +1,44 @@
+/*
+    Copyright 2022 JOJO Exchange
+    SPDX-License-Identifier: BUSL-1.1
+*/
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./DegenDealer.sol";
+
+pragma solidity ^0.8.19;
+
+contract DegenDepositHelper is Ownable {
+    using SafeERC20 for IERC20;
+
+    address public immutable usdc;
+    address public immutable degenDealer;
+
+    mapping(address => bool) public adminWhiteList;
+
+    event UpdateAdmin(address admin, bool isValid);
+    event PerpDepositToDegen(address from, address to, uint256 amount);
+
+    constructor(address _usdc, address _degenDealer) Ownable() {
+        usdc = _usdc;
+        degenDealer = _degenDealer;
+    }
+
+    modifier onlyAdminWhiteList() {
+        require(adminWhiteList[msg.sender], "caller is not in the admin white list");
+        _;
+    }
+
+    function setWhiteList(address admin, bool isValid) external onlyOwner {
+        adminWhiteList[admin] = isValid;
+        emit UpdateAdmin(admin, isValid);
+    }
+
+    function depositToDegenDealer(address from, address to) external onlyAdminWhiteList {
+        uint256 usdcBalance = IERC20(usdc).balanceOf(address(this));
+        require(usdcBalance > 0, "need to transfer usdc first");
+        IERC20(usdc).approve(degenDealer, usdcBalance);
+        DegenDealer(degenDealer).deposit(to, usdcBalance);
+        emit PerpDepositToDegen(from, to, usdcBalance);
+    }
+}

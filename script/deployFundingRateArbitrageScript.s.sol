1be2238a13e7c12803577df8a7dc1615955a50af
JoscelynFarr <fushikun307@outlook.com>
2024-04-19 14:53:47 +0800

    update script

--- script/deployFundingRateArbitrageScript.s.sol
+++ script/deployFundingRateArbitrageScript.s.sol
@@ -1,29 +1,29 @@
-// SPDX-License-Identifier: GPL-2.0-or-later
+// // SPDX-License-Identifier: GPL-2.0-or-later
 
-pragma solidity ^0.8.19;
+// pragma solidity ^0.8.19;
 
-import "forge-std/Script.sol";
-import "../src/FundingRateArbitrage.sol";
+// import "forge-std/Script.sol";
+// import "../src/FundingRateArbitrage.sol";
 
-contract FundingRateArbitrageScript is Script {
-    // add this to be excluded from coverage report
-    function test() public { }
+// contract FundingRateArbitrageScript is Script {
+//     // add this to be excluded from coverage report
+//     function test() public { }
 
-    function run() external {
-        uint256 deployerPrivateKey = vm.envUint("JOJO_DEPLOYER_PK");
-        vm.startBroadcast(deployerPrivateKey);
-        new FundingRateArbitrage(
-            //collateral
-            0x85CB137033DffD36B7B32048C2Ec42cf39cf2ee5,
-            //bank
-            0xb0D9Ce393f3483449be357EF715a3492858f8a5E,
-            //dealer
-            0xFfD3B82971dAbccb3219d16b6EB2DB134bf55300,
-            //perpmarket
-            0xFeAdd00ac346468B30AB59b964Be060Da7272dC6,
-            //operator
-            0xF1D7Ac5Fd1b806d24bCd2764C7c29A9fAd51698B
-        );
-        vm.stopBroadcast();
-    }
-}
+//     function run() external {
+//         uint256 deployerPrivateKey = vm.envUint("JOJO_BASE_TEST_DEPLOYER_PK");
+//         vm.startBroadcast(deployerPrivateKey);
+//         new FundingRateArbitrage(
+//             //collateral
+//             0xdc8f523692A88E938fbab280eD6322927E39CcE8,
+//             //bank
+//             0xeE1160ac170bad71C7651206c33943757e17F93a,
+//             //dealer
+//             0x65bE09345311aCc72d9358Ea7d7B13A91DFF51B6,
+//             //perpmarket
+//             0xC783678d996A480b58fdf7Fa355dead816C7DD75,
+//             //operator
+//             0xF1D7Ac5Fd1b806d24bCd2764C7c29A9fAd51698B
+//         );
+//         vm.stopBroadcast();
+//     }
+// }

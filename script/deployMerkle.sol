9b550d6ddd8146bbefe770300b6802a4676242a4
radar bear <leimingda94@gmail.com>
2024-09-19 02:19:43 -0400

    update deployMerkle

--- script/deployMerkle.sol
+++ script/deployMerkle.sol
@@ -40,3 +40,38 @@ contract DeployMerkleDistributor is Script {
         Utils.logInputs(inputs);
     }
 }
+
+contract DeployMerkleDistributorTest is Script {
+    // add this to be excluded from coverage report
+    function test() public { }
+
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("JOJO_DEPLOYER_PK");
+        vm.startBroadcast(deployerPrivateKey);
+        address token = 0xA42589C591f8AE08B0e2C2C18439d72628a66c3E;
+        bytes32 root = 0x96a180650761c384ed626edc85938b9af8f437fc9eaaa42fd1b508623b844099;
+        uint256 endTime = 1827740800;
+        MerkleDistributorWithDeadline distributor = new MerkleDistributorWithDeadline(
+            // token
+            token,
+            // root
+            root,
+            // timestamp
+            endTime
+        );
+        vm.stopBroadcast();
+
+        string memory chainId = vm.envString("CHAIN_ID");
+        bytes memory arguments = abi.encode(token,root,endTime);
+        string[] memory inputs = new string[](8);
+        inputs[0] = "forge";
+        inputs[1] = "verify-contract";
+        inputs[2] = Utils.addressToString(address(distributor));
+        inputs[3] = "src/token/MerkleDistributorWithDeadline.sol:MerkleDistributorWithDeadline";
+        inputs[4] = "--chain-id";
+        inputs[5] = chainId;
+        inputs[6] = "--constructor-args";
+        inputs[7] = Utils.bytesToStringWithout0x(arguments);
+        Utils.logInputs(inputs);
+    }
+}
